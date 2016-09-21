c50402784299eccec8d7553402f39852ee4d6c7a
zak <zak.j.holt@gmail.com>
2016-09-20 22:39:51 -0400

    pointed to mlab

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
     global.DATABASE_URL ||
-    (process.env.NODE_ENV === 'production' ?
+    (process.env.NODE_ENV === 'mongodb://<dbuser>:<dbpassword>@ds044989.mlab.com:44989/mongo-shopping-list' ?
         'mongodb://localhost/shopping-list' :
         'mongodb://localhost/shopping-list-dev');
 exports.PORT = process.env.PORT || 8080;
